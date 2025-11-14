8f360fa5421dab93a00a096fb65be2b8ec68378b
nitish1445 <sarainitish@gmail.com>
2025-11-14 20:28:01 +0530

    basic JS

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,22 @@
+var a = 10;
+
+var a = 20;
+
+let b = 20;
+
+console.log(a, b);
+
+let ar = ["abc", 1234, 45.84, "a", true];
+console.log(ar);
+
+console.warn("this is warning");
+console.error("ye error hai");
+
+alert("this is alert");
+
+if(confirm("Are you sure"))
+{
+    console.log("thanks");
+}else{
+    console.log("its ok");
+}
